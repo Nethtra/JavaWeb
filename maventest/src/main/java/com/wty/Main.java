e59159b59ab6f583e151c90bd884f04508c5147a
wty <thieisanemail@example.com>
2024-05-07 17:17:50 +0800

    second

--- maventest/src/main/java/com/wty/Main.java
+++ maventest/src/main/java/com/wty/Main.java
@@ -1,14 +1,14 @@
 package com.wty;
 
+import org.slf4j.Logger;
+
 /**
  * @author 王天一
  * @version 1.0
- */// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+ */
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        Logger log;
         System.out.printf("Hello and welcome!");
 
         // Press F5 or click the green arrow button in the gutter to run the code.
